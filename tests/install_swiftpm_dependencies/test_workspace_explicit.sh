85dcd909543a9bed585a937f13e2d589bc0d4ad8
Gio Lodi <gio@mokacoding.com>
2024-08-09 16:23:02 +1000

    Add tests for `install_swiftpm_dependencies` with `xcworkspace`

---
+++ tests/install_swiftpm_dependencies/test_workspace_explicit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+
+TESTS_LOCATION="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+NEW_PATH=$PATH:"$TESTS_LOCATION/../../bin"
+export PATH=$NEW_PATH
+
+pushd "$TESTS_LOCATION/workspace"
+
+install_swiftpm_dependencies --workspace Demo.xcworspace
+
+xcodebuild test \
+  -scheme Demo \
+  -configuration Debug \
+  -destination 'platform=iOS Simulator' \
+  | xcbeautify
