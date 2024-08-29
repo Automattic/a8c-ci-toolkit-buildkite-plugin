80cc173eeabcf5cbe6529bf75e3d4311cf3b4b8e
Gio Lodi <gio@mokacoding.com>
2024-08-29 18:58:29 +1000

    Code-generate project

---
+++ tests/install_swiftpm_dependencies/test_scripts/test_project_2_automatic.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -eu
+
+set -o pipefail
+
+TESTS_LOCATION="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+NEW_PATH=$PATH:"$TESTS_LOCATION/../../../bin"
+export PATH=$NEW_PATH
+
+pushd "$TESTS_LOCATION/../project-2"
+
+brew install xcodegen
+
+xcodegen generate
+
+install_swiftpm_dependencies
+
+xcodebuild test \
+  -scheme Demo \
+  -configuration Debug \
+  -destination 'platform=iOS Simulator' \
+  | xcbeautify
