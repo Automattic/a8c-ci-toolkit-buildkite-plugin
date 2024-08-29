a36ed9f8ac7be269603eb7b48d1860c8ceb3cefc
Gio Lodi <gio@mokacoding.com>
2024-08-29 19:33:46 +1000

    Fix path in `test_workspace_and_project_automatic.sh`

--- tests/install_swiftpm_dependencies/test_scripts/test_workspace_and_project_automatic.sh
+++ tests/install_swiftpm_dependencies/test_scripts/test_workspace_and_project_automatic.sh
@@ -6,7 +6,7 @@ TESTS_LOCATION="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 NEW_PATH=$PATH:"$TESTS_LOCATION/../../../bin"
 export PATH=$NEW_PATH
 
-pushd "$TESTS_LOCATION/workspace_and_project"
+pushd "$TESTS_LOCATION/../workspace_and_project"
 
 install_swiftpm_dependencies
 
