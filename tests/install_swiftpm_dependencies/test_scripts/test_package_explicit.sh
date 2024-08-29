79e8cb4bdf83127c52af8fea8ed7557ab51874d0
Gio Lodi <gio@mokacoding.com>
2024-08-29 19:48:25 +1000

    Fix code-gen Buildkite script paths

--- tests/install_swiftpm_dependencies/test_scripts/test_package_explicit.sh
+++ tests/install_swiftpm_dependencies/test_scripts/test_package_explicit.sh
@@ -2,12 +2,16 @@
 
 set -o pipefail
 
+echo "--- :computer: Prepare environment"
 TESTS_LOCATION="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 NEW_PATH=$PATH:"$TESTS_LOCATION/../../../bin"
 export PATH=$NEW_PATH
 
+echo "--- :computer: Jump to test folder"
 pushd "$TESTS_LOCATION/../package"
 
+echo "--- :wrench: Run install_swiftpm_dependencies"
 install_swiftpm_dependencies --use-spm
 
+echo "--- :xcode: Run tests to verify packages have been fetched and are available"
 swift test
--- tests/install_swiftpm_dependencies/test_scripts/test_project_explicit.sh
+++ tests/install_swiftpm_dependencies/test_scripts/test_project_explicit.sh
@@ -2,14 +2,25 @@
 
 set -o pipefail
 
+echo "--- :computer: Prepare environment"
 TESTS_LOCATION="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 NEW_PATH=$PATH:"$TESTS_LOCATION/../../../bin"
 export PATH=$NEW_PATH
 
+echo "--- :computer: Jump to test folder"
 pushd "$TESTS_LOCATION/../project"
 
-install_swiftpm_dependencies --project Demo.xcodeproj
+brew install xcodegen
 
+xcodegen generate
+# Also resolve packages to generate Package.resolved in expected location
+PROJECT=Demo.xcodeproj
+xcodebuild -resolvePackageDependencies -project "$PROJECT"
+
+echo "--- :wrench: Run install_swiftpm_dependencies"
+install_swiftpm_dependencies --project "$PROJECT"
+
+echo "--- :xcode: Run tests to verify packages have been fetched and are available"
 xcodebuild test \
   -scheme Demo \
   -configuration Debug \
