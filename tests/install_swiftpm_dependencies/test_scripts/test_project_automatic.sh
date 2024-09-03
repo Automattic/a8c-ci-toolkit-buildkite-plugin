0bd7b1189da5ce1b748284cbe8ed7ee9118dee9d
Gio Lodi <gio@mokacoding.com>
2024-09-03 14:14:54 +1000

    Use a fixture `Package.resolved`

--- tests/install_swiftpm_dependencies/test_scripts/test_project_automatic.sh
+++ tests/install_swiftpm_dependencies/test_scripts/test_project_automatic.sh
@@ -7,10 +7,15 @@ source "$(dirname "${BASH_SOURCE[0]}")/set_up_environment.sh"
 echo "--- :computer: Jump to test folder"
 pushd "$TESTS_LOCATION/../project"
 
+echo "--- :computer: Generate project"
 brew install xcodegen
 make
-# Also resolve packages to generate Package.resolved in expected location
-xcodebuild -resolvePackageDependencies -project Demo.xcodeproj
+
+echo "--- :computer: Copy Package.resolved fixture"
+PROJECT="Demo.xcodeproj"
+XCODE_SPM_PATH="$PROJECT/project.xcworkspace/xcshareddata/swiftpm"
+mkdir -p "$XCODE_SPM_PATH"
+cp "$TESTS_LOCATION/../package_resolved_fixtures/valid.resolved" "$XCODE_SPM_PATH/Package.resolved"
 
 echo "--- :wrench: Run install_swiftpm_dependencies"
 install_swiftpm_dependencies
--- tests/install_swiftpm_dependencies/test_scripts/test_project_explicit.sh
+++ tests/install_swiftpm_dependencies/test_scripts/test_project_explicit.sh
@@ -7,13 +7,17 @@ source "$(dirname "${BASH_SOURCE[0]}")/set_up_environment.sh"
 echo "--- :computer: Jump to test folder"
 pushd "$TESTS_LOCATION/../project"
 
+echo "--- :computer: Generate project"
 brew install xcodegen
 make
-# Also resolve packages to generate Package.resolved in expected location
-PROJECT=Demo.xcodeproj
-xcodebuild -resolvePackageDependencies -project "$PROJECT"
+
+echo "--- :computer: Copy Package.resolved fixture"
+PROJECT="Demo.xcodeproj"
+XCODE_SPM_PATH="$PROJECT/project.xcworkspace/xcshareddata/swiftpm"
+mkdir -p "$XCODE_SPM_PATH"
+cp "$TESTS_LOCATION/../package_resolved_fixtures/valid.resolved" "$XCODE_SPM_PATH/Package.resolved"
 
 echo "--- :wrench: Run install_swiftpm_dependencies"
-install_swiftpm_dependencies --project "$PROJECT"
+install_swiftpm_dependencies --project $PROJECT
 
 "$TESTS_LOCATION/run_tests_with_xcodebuild.sh"
