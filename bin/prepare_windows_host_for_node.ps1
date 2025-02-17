a7e8550aa2ea01d69374a224cdd59e372b793490
Gio Lodi <gio@mokacoding.com>
2025-02-17 15:23:32 +1100

    DRY new PS1 name in deprecated script

--- bin/prepare_windows_host_for_node.ps1
+++ bin/prepare_windows_host_for_node.ps1
@@ -1,8 +1,10 @@
 # Stop script execution when a non-terminating error occurs
 $ErrorActionPreference = "Stop"
 
-Write-Host "+++ :warning: This command is deprecated"
-Write-Host "Please use prepare_windows_host_for_app_distribution.ps1 instead"
-Write-Host "Now calling prepare_windows_host_for_app_distribution.ps1..."
+$newScript = "prepare_windows_host_for_app_distribution.ps1"
 
-& "$PSScriptRoot\prepare_windows_host_for_app_distribution.ps1"
+Write-Host "+++ :warning: This command is deprecated"
+Write-Host "Please use $newScript instead"
+Write-Host "Now calling $newScript..."
+
+& "$PSScriptRoot\$newScript"
