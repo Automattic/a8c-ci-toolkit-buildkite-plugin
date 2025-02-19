eb9c63d8a4be7cf0925a280b3da963a21d068f82
Gio Lodi <gio.lodi@automattic.com>
2025-02-19 16:51:04 +1100

    Refined test for Windows 10 SDK
    Tested it by running on my Windows VM first this time.

--- tests/test-install-windows-10-sdk.ps1
+++ tests/test-install-windows-10-sdk.ps1
@@ -5,17 +5,23 @@ param (
 
 $ErrorActionPreference = "Stop"
 
+Write-Output "Running test-install-windows-10-sdk.ps1 with ExpectedExitCode=$ExpectedExitCode and ExpectedErrorKeyphrase=$ExpectedErrorKeyphrase"
+
 if (($ExpectedExitCode -eq 0) -and ($ExpectedErrorKeyphrase -ne "")) {
-  Write-Error "Expected call to succeed, but given an error keyphrase to check."
+  Write-Output "Expected call to succeed, but given an error keyphrase to check."
   exit 1
 }
 
-$output = "$PSScriptRoot\..\bin\install_windows_10_sdk.ps1 -DryRun"
+$output = & "$PSScriptRoot\..\bin\install_windows_10_sdk.ps1" -DryRun
 $exitCode = $LASTEXITCODE
 
 if ($exitCode -ne $ExpectedExitCode) {
-  Write-Error "Expected exit code $ExpectedExitCode, got $exitCode"
+  Write-Output "Expected exit code $ExpectedExitCode, got $exitCode"
+  Write-Output "Output was:"
+  Write-Output "$output"
   exit 1
+} else {
+  Write-Output "✅ Exit code matches expected value ($ExpectedExitCode)"
 }
 
 # Only check error keyphrase if exit code is not 0
@@ -23,8 +29,17 @@ if ($exitCode -eq 0) {
   exit 0
 }
 
+# If keyphrase is empty, assume the caller is satisfied with only testing the exit code
+if ($ExpectedErrorKeyphrase -eq "") {
+  Write-Output "Exit code match expectation and no error keyphrase was provided. Test completed."
+  exit 0
+}
+
 if ($output -notmatch $ExpectedErrorKeyphrase) {
-  Write-Error "Expected error to contain '$ExpectedErrorKeyphrase', but got:"
-  Write-Error "$output"
+  Write-Output "Expected error to contain '$ExpectedErrorKeyphrase', but got:"
+  Write-Output "$output"
   exit 1
+} else {
+  Write-Output "✅ Error keyphrase matches expected value ($ExpectedErrorKeyphrase)"
+  Write-Output "Test completed."
 }
