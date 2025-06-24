e7fd431d468ea949fea7ebb212109e1c97469cc7
Gio Lodi <gio@mokacoding.com>
2025-06-25 07:21:45 +1000

    Add another example

--- .buildkite/commands/command-that-calls-cmd-not-installed.ps1
+++ .buildkite/commands/command-that-calls-cmd-not-installed.ps1
@@ -1,6 +1,8 @@
 $ErrorActionPreference = "Stop"
 
-false
+# This should fail because npm is not available out of the box on our Windows
+# CI in 06/2025
+npm
 
 if ($LASTEXITCODE -ne 0) {
     Write-Host "Script failed with exit code $LASTEXITCODE"
