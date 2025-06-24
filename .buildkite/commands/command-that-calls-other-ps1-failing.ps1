56baab69c1d131810020063a640b39f368e25aa7
Gio Lodi <gio@mokacoding.com>
2025-06-25 06:48:55 +1000

    Add LASTEXITCODE check

--- .buildkite/commands/command-that-calls-other-ps1-failing.ps1
+++ .buildkite/commands/command-that-calls-other-ps1-failing.ps1
@@ -1,1 +1,10 @@
+# Alone, this does not fail
+# & "$PSScriptRoot\command-that-fails-internally.ps1"
+
+# To make the script fail, we need to check the error
 & "$PSScriptRoot\command-that-fails-internally.ps1"
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Script failed with exit code $LASTEXITCODE"
+    exit $LASTEXITCODE
+}
