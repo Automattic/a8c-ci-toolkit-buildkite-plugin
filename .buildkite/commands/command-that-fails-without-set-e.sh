75adc18184d12ebe31bfa858aec9433a302a107c
Gio Lodi <gio@mokacoding.com>
2025-06-25 10:56:47 +1000

    Update a log to explain success state

--- .buildkite/commands/command-that-fails-without-set-e.sh
+++ .buildkite/commands/command-that-fails-without-set-e.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "I'm about to exit 2. I expect this step to fail..."
+echo "I'm about to exit 2. I expect this step to pass if called directly, because the script does not set the -e flag."
 exit 2
