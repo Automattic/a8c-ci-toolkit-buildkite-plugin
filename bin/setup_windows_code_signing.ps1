82d0900a764f9520fa62345f7c86bd86fe0ee1c5
Gio Lodi <gio@mokacoding.com>
2025-12-03 17:49:47 +1100

    Reformat `setup_windows_code_signing.ps1`
    I opened the file to check for obsolete calls and used the occasion to
    update it this way.

--- bin/setup_windows_code_signing.ps1
+++ bin/setup_windows_code_signing.ps1
@@ -1,17 +1,24 @@
-# Installs the code signing certificate for Windows app distribution.
-#
-# The certificate is stored in our AWS SecretsManager storage (`windows-code-signing-certificate` secret ID).
-# It is decoded and stored in the `certificate.pfx` file.
+<#
+  .SYNOPSIS
+    Installs the code signing certificate for Windows app distribution.
+
+    The certificate is stored in our AWS SecretsManager storage (`windows-code-signing-certificate` secret ID).
+    It is decoded and stored in the `certificate.pfx` file.
+#>
 
 # Stop script execution when a non-terminating error occurs
 $ErrorActionPreference = "Stop"
 
 Write-Output "--- :lock_with_ink_pen: Download Code Signing Certificate"
+
 $certificateBinPath = "certificate.bin"
+
 $EncodedText = aws secretsmanager get-secret-value --secret-id windows-code-signing-certificate `
   | jq -r '.SecretString' `
   | Out-File $certificateBinPath
+
 $certificatePfxPath = "certificate.pfx"
+
 certutil -decode $certificateBinPath $certificatePfxPath
 
 If ($LastExitCode -ne 0) {
